05e3658b45495bf4bfdd31f760650d49507dd711
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-02-22 21:18:37 +0530

    Color changes

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,13 +9,11 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
   const GradientContainer.purple({super.key})
-      : color1 = Colors.deepPurple,
+      : color1 = const Color.fromARGB(255, 123, 56, 241),
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
-
-
 
   @override
   Widget build(context) {
